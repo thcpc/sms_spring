e08b554a3c17891a73a8614f6e61877f0034288a
thcpc <redcpc@163.com>
2022-06-13 12:50:10 +0800

    What: Add the step02

---
+++ spring-step-02/src/main/java/org/springframework/beans/factory/support/AbstractAutowireCapableBeanFactory.java
@@ -0,0 +1,18 @@
+package org.springframework.beans.factory.support;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanDefinition;
+
+public abstract class AbstractAutowireCapableBeanFactory extends AbstractBeanFactory{
+    @Override
+    protected Object createBean(String beanName, BeanDefinition beanDefinition) throws BeansException {
+        Object bean;
+        try {
+            bean = beanDefinition.getBeanClass().newInstance();
+        } catch (InstantiationException | IllegalAccessException e) {
+            throw new BeansException("Instantiation of bean failed", e);
+        }
+        this.addSingleton(beanName, bean);
+        return bean;
+    }
+}
