5fd12cf02643bd6a9b2e953c7934a786b05f0dbd
thcpc <redcpc@163.com>
2022-06-23 13:31:08 +0800

    What: init step06

---
+++ spring-step-06/src/main/java/org/springframework/beans/factory/config/BeanFactoryPostProcessor.java
@@ -0,0 +1,13 @@
+package org.springframework.beans.factory.config;
+
+import org.springframework.beans.BeansException;
+
+public interface BeanFactoryPostProcessor {
+    /**
+     * 在所有的 BeanDefinition 加载完成后，实例化 Bean 对象之前，提供修改 BeanDefinition 属性的机制
+     *
+     * @param beanFactory
+     * @throws BeansException
+     */
+    void postProcessBeanFactory() throws BeansException;
+}
