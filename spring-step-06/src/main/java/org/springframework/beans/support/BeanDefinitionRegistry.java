65f9cc23dc5b1c188e7bc9bb636641cd51f765c2
thcpc <redcpc@163.com>
2022-06-24 13:22:17 +0800

    What: update step06

---
+++ spring-step-06/src/main/java/org/springframework/beans/support/BeanDefinitionRegistry.java
@@ -0,0 +1,15 @@
+package org.springframework.beans.support;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanDefinition;
+
+public interface BeanDefinitionRegistry {
+
+    String[] getBeanDefinitionNames();
+
+    void registerBeanDefinition(String beanName, BeanDefinition beanDefinition);
+
+    BeanDefinition getBeanDefinition(String beanName) throws BeansException;
+
+    boolean containBeanDefinition(String beanName);
+}
